f85b6af9e06559d067510a2537075af7f0e58687
Secplavory <zxcvbnm9996666@gmail.com>
2021-08-31 14:21:19 +0800

    完成canvas rwd

--- pm/src/components/painManagePage/PainManagePage.js
+++ pm/src/components/painManagePage/PainManagePage.js
@@ -2,14 +2,33 @@ import './PainManagePage.scss'
 import * as THREE from 'three'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 
 function PainManagePage() {
+  const canvasContent = CanvasInit();
+  OnCanvasResize(canvasContent);
+
+  return (
+    <div id="PainCanvas">
+      <footer></footer>
+    </div>
+  );
+}
+
+function CanvasInit(){
+  const [canvasContent, setCanvasContent] = useState({
+    scene: undefined,
+    camera: undefined,
+    renderer: undefined,
+    canvas: undefined,
+    directionalLight: undefined,
+    controls: undefined
+  });
   useEffect(()=>{
-    const scene = new THREE.Scene();
-    const camera = new  THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer();
     const canvas = document.getElementById("PainCanvas");
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth/canvas.clientHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
     renderer.setSize(canvas.clientWidth, canvas.clientHeight);
     renderer.setClearColor('white', 0.3);
     canvas.appendChild(renderer.domElement);
@@ -21,15 +40,16 @@ function PainManagePage() {
     controls.update();
 
     const loader = new GLTFLoader();
+    var body;
     loader.load(
       'male_body_base_mesh/scene.gltf',
       function(gltf){
-        var body = gltf.scene;
+        body = gltf.scene;
         body.position.set(0, 1.2, 0);
+        body.name = "body";
         scene.add(body);
       }
     )
-
     var animate = function () {
       requestAnimationFrame( animate );
 
@@ -40,13 +60,32 @@ function PainManagePage() {
       renderer.render( scene, camera );
     };
     animate();
+    setCanvasContent({
+      scene: scene,
+      camera: camera,
+      renderer: renderer,
+      canvas: canvas,
+      directionalLight: directionalLight,
+      controls: controls
+    });
   }, []);
 
-  return (
-    <div id="PainCanvas">
-      <footer></footer>
-    </div>
-  );
+  return canvasContent;
+}
+function OnCanvasResize(canvasContent){
+  const renderer = canvasContent.renderer;
+  const canvas = canvasContent.canvas;
+  const camera = canvasContent.camera;
+  useEffect(()=>{
+    function handleResize(){
+      if(renderer !== undefined && canvas !== undefined && camera !== undefined){
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+      }
+    }
+    window.addEventListener("resize", handleResize);
+  }, [renderer, canvas, camera]);
 }
 
 
